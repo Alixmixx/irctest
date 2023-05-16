2501729e8452f8327c193aa082d0a6db826f9f85
Axel Brisse <axel.brisse@gmail.com>
2023-05-16 12:48:28 +0200

    proxy.py works kinda

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# Manual testing
+# Manual multiclient testing
 
 1. 1st terminal : `make && valgrind --track-origins=yes ./ircserv 6667`
 2. 2nd terminal : `go run scripts/client.go`
 3. 2nd terminal : `go run scripts/client.go & go run scripts/client.go & go run scripts/client.go`
 4. 1st terminal : Ctrl+C or `quit`
+
+# Proxy usage
+
+1. 1st terminal : `make && valgrind --track-origins=yes ./ircserv 6667`
+2. 2nd terminal : `python3 scripts/proxy.py localhost:6667 5555`
+3. 3rd terminal : `irssi -c localhost -p 5555`
+3. 3rd terminal : `/whois <username>`
--- scripts/proxy.py
+++ scripts/proxy.py
@@ -1,66 +1,99 @@
+import errno
+import os
+import re
+import signal
 import socket
 import sys
-from _thread import start_new_thread
+from termcolor import colored
 
-RESET = "\u001b[0m"
-RED = "\u001b[31m"
-GREEN = "\u001b[32m"
-BLUE = "\u001b[34m"
+BUFFER_SIZE = 1024
+BACKLOG = 128
 
-PROXY_PORT = 5556
-PROXY_HOST = ""
-SERVER_PORT = 6668
-SERVER_HOST = ""
+def is_valid_address(s):
+    return re.fullmatch(r".*:\d{4,5}", s)
 
-BACKLOG = 8
-BUFFER_SIZE = 512
+def parse_address(s):
+    host, port = s.split(":")
+    return host, int(port)
 
-def proxy_server(conn_server, addr_server, data):
-    print("c->s |", data)
-    should_exit = False
-    try:
-        sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock_client.connect((SERVER_HOST, SERVER_PORT))
-        sock_client.send(data)
-        while True:
-            reply = sock_client.recv(BUFFER_SIZE)
-            print("s->c |", reply)
-            if len(reply) > 0:
-                conn_server.send(reply)
-            else:
-                break
-    except Exception as e:
-        print(f"{RED}proxy_server, {e}{RESET}")
-        should_exit = True
-    sock_client.close()
-    conn_server.close()
-    if should_exit:
-        sys.exit(1)
-
-try:
-    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_server.bind((PROXY_HOST, PROXY_PORT))
-    sock_server.listen(BACKLOG)
-    print(f"{GREEN}Proxy listening on port {PROXY_PORT}{RESET}")
-except Exception as e:
-    print(f"{RED}Unable to Initialize Socket{RESET}")
-    sock_server.close()
+if len(sys.argv) != 3 or not re.fullmatch(r".*:\d{4,5}", sys.argv[1]) or not re.fullmatch(r"\d{4,5}", sys.argv[2]):
+    print(f"Usage: python3 {sys.argv[0]} server_host:server_port proxy_port")
+    print(f"Example: python3 {sys.argv[0]} localhost:6669 5555")
     sys.exit(1)
-while True:
+SERVER_ADDRESS = SERVER_HOST, SERVER_PORT = parse_address(sys.argv[1])
+PROXY_ADDRESS = PROXY_HOST, PROXY_PORT = "localhost", int(sys.argv[2])
+
+def print_info(message):
+    print(colored(message, "blue"), end="\n\n")
+
+def tryclose(sock):
     try:
-        conn_server, addr_server = sock_server.accept()
-        data = b""
-        while True:
-            buf = conn_server.recv(BUFFER_SIZE)
-            if not buf:
-                break
-            data += buf
-        start_new_thread(proxy_server, (conn_server, addr_server, data))
-    except KeyboardInterrupt:
-        sock_server.close()
-        print(f"{GREEN}\rGood bye.{RESET}")
-        sys.exit(0)
-    except Exception as e:
-        sock_server.close()
-        print(f"{RED}\rGood bye.\n{e}{RESET}")
-        sys.exit(1)
+        sock.close()
+    except:
+        print_info(f"Could not close socket {sock}")
+
+def parent_sigint(sig, frame):
+    print
+    print_info("\rGood bye.")
+    tryclose(server_socket)
+    tryclose(proxy_socket)
+    sys.exit(0)
+
+def child_sigint(sig, frame):
+    tryclose(server_socket)
+    tryclose(client_socket)
+    sys.exit(0)
+
+def grim_reaper(signum, frame):
+    while True:
+        try:
+            pid, status = os.waitpid(-1, os.WNOHANG)
+        except OSError:
+            return
+        if pid == 0:
+            return
+
+def parent_signals():
+    signal.signal(signal.SIGINT, parent_sigint)
+    signal.signal(signal.SIGCHLD, grim_reaper)
+
+def child_signals():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    signal.signal(signal.SIGCHLD, signal.SIG_DFL)
+
+def forward_data(sender, receiver, color):
+    data = sender.recv(BUFFER_SIZE)
+    if data:
+        print(colored(f"{sender.getsockname()[1]} to {receiver.getsockname()[1]}:", color))
+        print(colored(data.decode(), color))
+        receiver.sendall(data)
+
+server_socket = proxy_socket = client_socket = None
+parent_signals()
+
+proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+proxy_socket.bind(PROXY_ADDRESS)
+proxy_socket.listen(BACKLOG)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.connect(SERVER_ADDRESS)
+
+print_info(f"Proxy listening on port {PROXY_PORT}, talking to {SERVER_PORT}.")
+while True:
+    client_socket, client_address = proxy_socket.accept()
+    print_info(f"Client :{client_address[1]} connected.")
+    pid = os.fork()
+    if pid == 0:
+        child_signals()
+        proxy_socket.close()
+        if os.fork() == 0:
+            while True:
+                forward_data(client_socket, server_socket, "green")
+        elif os.fork() == 0:
+            while True:
+                forward_data(server_socket, client_socket, "red")
+        else:
+            os.waitpid(-1, os.WNOHANG)
+            os.waitpid(-1, os.WNOHANG)
+    else:
+        client_socket.close()
