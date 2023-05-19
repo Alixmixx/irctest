db57d124a79bb5970f3e9c4e00d04cde531e2fdc
Axel Brisse <axel.brisse@gmail.com>
2023-05-19 05:28:22 +0200

    broadcast calls reply

--- srcs/Message/Reply.cpp
+++ srcs/Message/Reply.cpp
@@ -2,12 +2,10 @@
 
 void Client::reply(std::string replyMessage) const
 {
-	replyMessage += "\r\n";
-
 	if (DEBUG)
-		std::cout << "\033[1;32mMessage to client: " << _clientSocket << "\n"
-				  << replyMessage << "\033[0m" << std::endl;
+		std::cout << "\033[1;32mMessage to client: " << _clientSocket << "\n" << replyMessage << "\033[0m" << std::endl;
 
+	replyMessage += "\r\n";
 	send(_clientSocket, replyMessage.c_str(), replyMessage.length(), MSG_NOSIGNAL);
 }
 
