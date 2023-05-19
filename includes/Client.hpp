669b0aa46b0bd0beb91c41e70181c05c130f0bc5
Axel Brisse <axel.brisse@gmail.com>
2023-05-19 09:41:41 +0200

    simplify handleQuit

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -10,7 +10,6 @@ public:
 	Client(Server* server, int socketFd, sockaddr_in clientAddress);
 	~Client();
 
-	// Setters
 	void setNickname(std::string nickname);
 	void setIsRegistered(bool isRegistered);
 	void setUsername(std::string username);
@@ -22,7 +21,6 @@ public:
 	void addChannel(Channel* channel);
 	void leaveChannel(Channel* channel);
 
-	// Getters
 	int			getSocket() const;
 	int			getChannelCount() const;
 	bool		IsRegistered() const;
@@ -35,7 +33,6 @@ public:
 	std::string getMessage() const;
 	Server*		getServer() const;
 
-	// Reply
 	void reply(std::string replyMessage) const;
 	void reply(std::string replyMessage, ReplyCode replyCode) const;
 	void reply(ReplyCode replyCode, std::string arg1 = "", std::string arg2 = "", std::string arg3 = "", std::string arg4 = "") const;
--- srcs/commands/handleQuit.cpp
+++ srcs/commands/handleQuit.cpp
@@ -2,22 +2,10 @@
 
 void Server::handleQuit(Client* client, std::vector<std::string> arguments)
 {
-	std::string clientNickname = client->getNickname();
-	std::string clientMessage;
 	removeClient(client);
-
-	if (arguments.size() == 0)
-	{
-		broadcast(_clients, ":" + clientNickname + " QUIT");
-		return;
-	}
-
-	for (std::vector<std::string>::iterator it = arguments.begin(); it != arguments.end(); it++)
-	{
-		clientMessage += *it;
-		if (it + 1 != arguments.end())
-			clientMessage += " ";
-	}
-
-	broadcast(_clients, ":" + clientNickname + " QUIT " + clientMessage);
+	std::string clientMessage;
+	std::string quitMessage = ":" + client->getNickname() + " QUIT";
+	if (arguments.size() >= 1)
+		quitMessage += " " + arguments[0];
+	broadcast(_clients, quitMessage);
 }
