1082ed04f5c3c3cfe7701b3e3580592e3d2c53a6
Axel Brisse <axel.brisse@gmail.com>
2023-05-20 21:26:01 +0200

    better handlePing

--- srcs/commands/handlePing.cpp
+++ srcs/commands/handlePing.cpp
@@ -3,10 +3,7 @@
 void Server::handlePing(Client* client, std::vector<std::string> arguments)
 {
 	if (arguments.empty())
-	{
-		std::string asd;
 		client->reply(ERR_NEEDMOREPARAMS, "PING");
-		return;
-	}
-	client->reply("PONG " + arguments[0]);
+	else
+		client->reply(":" + _serverHostname + " PONG " + _serverHostname + " :" + arguments[0]);
 }
