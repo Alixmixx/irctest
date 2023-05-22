cb133bc1dc7b193f96dfb9307982c400f9278d35
amuller <alixmuller.am@gmail.com>
2023-05-22 23:00:16 +0200

    mode fix

--- srcs/commands/handleMode.cpp
+++ srcs/commands/handleMode.cpp
@@ -6,68 +6,72 @@ void Server::setModeChannel(Client *client, Channel *channel, std::vector<std::s
 {
 	// Find out if we're adding or removing modes iterate + 1
 	std::string replyString = "";
+	if (arguments[0][0] != '+' && arguments[0][0] != '-')
+		replyString += "+";
 	std::string modeString = arguments[1];
 	bool sign = PLUS;
-	bool addToString = false;
+	short addToString = 0;
 	int	first = 0;
 	unsigned int clientNumber = 2;
 
 	for (size_t i = 0; i < modeString.length(); i++)
 	{
-		addToString = true;
+		addToString = 0;
 		switch (modeString[i])
 		{
 			case '+':
-				sign = PLUS;
+				sign = PLUS; ++addToString;
 				break;
 			case '-':
-				sign = MINUS;
+				sign = MINUS; ++addToString;
 				break;
 			case 'i':
-				if (!(first & M_INVITE) && (first |= M_INVITE)) channel->setMode(M_INVITE, sign);
+				if (!(first & M_INVITE) && (first |= M_INVITE) && ++addToString) channel->setMode(M_INVITE, sign);
 				break;
 			case 'k':
-				if (!(first & M_KEY) && (first |= M_KEY)) channel->setMode(M_KEY, sign);
+				if (!(first & M_KEY) && (first |= M_KEY) && ++addToString) channel->setMode(M_KEY, sign);
 				break;
 			case 'l':
-				if (!(first & M_LIMITED) && (first |= M_LIMITED)) channel->setMode(M_LIMITED, sign);
+				if (!(first & M_LIMITED) && (first |= M_LIMITED) && ++addToString) channel->setMode(M_LIMITED, sign);
 				break;
 			case 'm':
-				if (!(first & M_MODERATED) && (first |= M_MODERATED)) channel->setMode(M_MODERATED, sign);
+				if (!(first & M_MODERATED) && (first |= M_MODERATED) && ++addToString) channel->setMode(M_MODERATED, sign);
 				break;
 			case 'o':
 			{
 				if (arguments.size() <= clientNumber)
 				{
-					addToString = false;
+					addToString = 0;
 					break;
 				}
 				Client *target = getClient(arguments[clientNumber]);
 				if (target == NULL || !channel->isOnChannel(target))
 				{
 					client->reply(ERR_USERNOTINCHANNEL, arguments[clientNumber]);
-					addToString = false;
+					addToString = 0;
 					break; 
 				}
 				if (sign == PLUS)
 					channel->setClientMode(target, OPERATOR);
 				else if (sign == MINUS)
 					channel->setClientMode(target, USER);
+				addToString = 1;
 				break;
 			}
 			case 's':
-				if (!(first & M_SECRET) && (first |= M_SECRET)) channel->setMode(M_SECRET, sign);
+				if (!(first & M_SECRET) && (first |= M_SECRET) && ++addToString) channel->setMode(M_SECRET, sign);
 				break;
 			case 't':
-				if (!(first & M_PROTECTED) && (first |= M_PROTECTED)) channel->setMode(M_PROTECTED, sign);
+				if (!(first & M_PROTECTED) && (first |= M_PROTECTED) && ++addToString) channel->setMode(M_PROTECTED, sign);
 				break;
 			default:
-				addToString = false;
+				addToString = 0; // client->reply(ERR_UNKNOWNMODE, modeString[i]);
 				break;
 		}
 		if (addToString)
 			replyString += modeString[i];
 	}
+	replyString = replyString.substr(0, replyString.find_last_not_of("+-") + 1);
 	broadcast(channel->getChannelUsers(), client->getPrefix() + " MODE " + channel->getName() + " " + replyString);
 }
 
